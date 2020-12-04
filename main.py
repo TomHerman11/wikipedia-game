f729b22112a4000faae7a8ba2ccbd563901577b7
Tom Herman <tomherman11@gmail.com>
2020-12-04 18:49:29 +0200

    ask the user to choose algorithm

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import time
+import asyncio
 
 from bfs import bfs
 from dfs import dfs
 from async_bfs import async_bfs
 from async_dfs import async_dfs
-import asyncio
+
+
+MAX_ARTICLES_TO_SEARCH = 1000000
+MAX_WIKI_PATH_LENGTH = 3  # including 'start' and 'goal'
 
 
 def printPath(path: list[str]) -> None:
@@ -12,34 +15,39 @@ def printPath(path: list[str]) -> None:
 
 
 async def main() -> None:
-    MAX_ARTICLES_TO_SEARCH = 1000000
-    MAX_WIKI_PATH_LENGTH = 3  # including 'start' and 'goal'
-
     # receive (and edit if needed) the start and goal articles
-    start_article = input("Please enter the start article: ").lower()
-    goal_article = input("Please enter the goal article: ").lower()
+    start_article = input("Please type the start article: ").lower()
+    goal_article = input("Please type the goal article: ").lower()
     if not start_article or not goal_article:
         return
+    algorithm = input("Please type which search algorithm should be used (BFS, DFS, async_BFS, async_DFS): ").lower()
 
     # *** BFS: ***
-    # printPath(
-    #     bfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
-    # )
+    if (algorithm == "bfs"):
+        printPath(
+            bfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
+        )
 
     # *** DFS: ***
-    # printPath(
-    #     dfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
-    # )
+    elif (algorithm == "dfs"):
+        printPath(
+            dfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
+        )
 
     # *** 'ASYNC' BFS: (please read function's documentation) ***
-    # printPath(
-    #     await async_bfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
-    # )
+    elif (algorithm == "async_bfs"):
+        printPath(
+            await async_bfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
+        )
 
     # *** 'ASYNC' DFS: (please read function's documentation) ***
-    # printPath(
-    #     await async_dfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
-    # )
+    elif (algorithm == "async_dfs"):
+        printPath(
+            await async_dfs(start_article, goal_article, MAX_ARTICLES_TO_SEARCH, MAX_WIKI_PATH_LENGTH)
+        )
+
+    else:
+        print("Please make sure you typed correctly which search algorithm should be used :)")
 
 if __name__ == "__main__":
     asyncio.run(main())
