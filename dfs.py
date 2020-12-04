ba99385cc54eb11f6919224fc088776b17bf5058
Tom Herman <tomherman11@gmail.com>
2020-12-04 17:56:26 +0200

    fix 'dfs' bugs

--- dfs.py
+++ dfs.py
@@ -10,20 +10,17 @@ def dfs(
 ) -> list[str]:
 
     def DFS_helper(curr_article: str, goal_article: str, path: list[str]) -> list[str]:
-        if curr_article in found:
-            return []
-
-        found.add(curr_article)
         path.append(curr_article)
-
         if curr_article == goal_article:
             return path
 
         if len(path) < MAX_WIKI_PATH_LENGTH:
             for link in fetch_wiki_article_and_get_neighboring_articles(curr_article):
-                curr_try = DFS_helper(link, goal_article, path)
-                if curr_try:
-                    return curr_try
+                if (len(found) < MAX_ARTICLES_TO_SEARCH) and (link not in found):
+                    found.add(link)
+                    curr_try = DFS_helper(link, goal_article, path)
+                    if curr_try:
+                        return curr_try
 
         path.pop()
         return []
